2e2811ee0506dbdee3553af2d010ea8c3be3e260
Vahidpro <ivahidp@gmail.com>
2024-01-21 17:29:13 +0330

    Added finish and restart quiz

--- src/App.js
+++ src/App.js
@@ -45,6 +45,8 @@ function reducer(state, action) {
 				status: "finished",
 				highscore: state.points > state.highscore ? state.points : state.highscore,
 			};
+		case "restart":
+			return { ...initialState, questions: state.questions, status: "ready" };
 		default:
 			throw new Error("Action Unknown");
 	}
@@ -105,6 +107,7 @@ export default function App() {
 						points={points}
 						maxPossiblePoints={maxPossiblePoints}
 						highscore={highscore}
+						dispatch={dispatch}
 					/>
 				)}
 			</Main>
