f5059a0a10036773990fd966fda27089fbbff8e1
Vahidpro <ivahidp@gmail.com>
2024-01-21 12:37:04 +0330

    Added progressbar and scores

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
 
 const initialState = {
 	questions: [],
@@ -42,12 +43,16 @@ function reducer(state, action) {
 }
 
 export default function App() {
-	const [{ questions, status, index, answer }, dispatch] = useReducer(
+	const [{ questions, status, index, answer, points }, dispatch] = useReducer(
 		reducer,
 		initialState
 	);
 
 	const numQuestions = questions.length;
+	const maxPossiblePoints = questions.reduce(
+		(prev, cur) => prev + cur.points,
+		0
+	);
 	useEffect(() => {
 		fetch("http://localhost:8000/questions")
 			.then((res) => res.json())
@@ -69,6 +74,13 @@ export default function App() {
 				)}
 				{status === "active" && (
 					<>
+						<Progress
+							index={index}
+							numQuestions={numQuestions}
+							points={points}
+							maxPossiblePoints={maxPossiblePoints}
+							answer={answer}
+						/>
 						<Question
 							question={questions[index]}
 							dispatch={dispatch}
